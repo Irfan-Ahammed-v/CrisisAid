96f1621e4e034c82ba45f8006943f3450fb274da
Irfan-Ahammed-v <irfanahammedinfo@gmail.com>
2026-02-11 21:56:13 +0530

    demo pending requests page addedd

--- Client/src/Router/CenterRouter.jsx
+++ Client/src/Router/CenterRouter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import CenterDashboard from "../Center/Pages/CenterDashboard/CenterDashboard";
+import PendingRequests from "../Center/Pages/Pendingrequests/Pendingrequests";
 
 
 
@@ -12,6 +13,7 @@ const CenterRouter  = () =>{
         <div>
     <Routes>
     <Route path="/" element={<CenterDashboard/>}/>
+    <Route path="/pending-requests" element={<PendingRequests/>}/>
     </Routes>
         </div>
     );
