6c80c63be872ac0ae10d642e87ebf6e527ee5ed2
Irfan-Ahammed-v <irfanahammedinfo@gmail.com>
2026-02-13 20:17:35 +0530

    addedd new roots

--- Client/src/Router/CenterRouter.jsx
+++ Client/src/Router/CenterRouter.jsx
@@ -4,6 +4,9 @@ import CenterDashboard from "../Center/Pages/CenterDashboard/CenterDashboard";
 import Requests from "../Center/Pages/Requests/Requests";
 import DisasterReports from "../Center/Pages/DisasterReport/DisasterReports";
 import VolunteerManagement from "../Center/Pages/VolunteerManagment/VolunteerManagment";
+import CampManagment from "../Center/Pages/CampManagment/CampManagment";
+import PendingApprovals from "../Center/Pages/PendingApprovals/PendingApprovals";
+import CenterProfile from "../Center/Pages/CenterProfile/CenterProfile";
 
 
 
@@ -19,6 +22,10 @@ const CenterRouter  = () =>{
     <Route path="/requests" element={<Requests/>}/>
     <Route path="/disaster-reports" element={<DisasterReports/>}/>
     <Route path="/volunteer-management" element={<VolunteerManagement/>}/>
+    <Route path="/camp-management" element={<CampManagment/>}/>
+    <Route path="/pending-approvals" element={<PendingApprovals/>}/>
+    <Route path="/profile" element={<CenterProfile/>}/>
+    <Route path="*" element={<div className="text-center py-10">Page Not Found</div>} />
     </Routes>
         </div>
     );
