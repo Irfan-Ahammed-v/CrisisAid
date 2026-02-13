f2cf8f36b6f6dc0740e0460ed660f0d62b7c0c3d
Irfan-Ahammed-v <irfanahammedinfo@gmail.com>
2026-02-13 19:02:57 +0530

    refactor: reorganize route definitions for better readability

--- Server/routes/camp.routes.js
+++ Server/routes/camp.routes.js
@@ -4,29 +4,34 @@ const { campRegister, getCentersByDistrict,home, getRequestitems, getPlaces, new
 const { campAuth } = require("../middlewares/campAuth");
 const upload = require("../middlewares/disasterProofUpload");
 const uploadCampProof = require("../middlewares/uploadCampProof");
-// const CampProof = require("../middlewares/uploadCampProof");
 
+//================================== POST================================
 router.post("/register", campRegister);
-router.get("/places",campAuth, getPlaces);
-router.get("/centers/:districtId",getCentersByDistrict)
-router.get("/home", campAuth, home)
-router.get("/request-items", campAuth, getRequestitems);
 router.post("/new-request", campAuth, newRequest);
-router.get("/request/:requestId", campAuth, getRequest);
 router.post(
   "/disaster",
   campAuth,
   upload.single("photo"), // field name MUST match frontend
   newdisaster
 );
-router.get("/requests", campAuth, getRequests);
-router.get("/profile", campAuth, getCampProfile);
-router.patch("/update-occupancy", campAuth, updateOccupancy);
-router.patch("/update-profile", campAuth, updateProfile);
 router.post(
   "/upload-proof",
   campAuth,
   uploadCampProof.single("camp_proof"),
   CampProof
 );
+
+//================================== GET================================
+router.get("/places",campAuth, getPlaces);
+router.get("/centers/:districtId",getCentersByDistrict)
+router.get("/home", campAuth, home)
+router.get("/request-items", campAuth, getRequestitems);
+router.get("/request/:requestId", campAuth, getRequest);
+router.get("/requests", campAuth, getRequests);
+router.get("/profile", campAuth, getCampProfile);
+
+//================================== PATCH================================
+router.patch("/update-occupancy", campAuth, updateOccupancy);
+router.patch("/update-profile", campAuth, updateProfile);
+
 module.exports = router;
